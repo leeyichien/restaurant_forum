aa5bc2528c7b5842cf6b6a906e0c6cfb649bae61
leeyichien <tdme.tdem@me.com>
2019-08-25 19:59:57 +0800

    Confirm user password and unique email

--- app.js
+++ app.js
@@ -2,9 +2,20 @@ const express = require("express");
 const handlebars = require("express-handlebars");
 const db = require("./models");
 const bodyParser = require("body-parser"); // add this
+const flash = require("connect-flash");
+const session = require("express-session");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 const port = 3000;
+
+app.use(session({ secret: "secret", resave: false, saveUninitialized: false }));
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash("success_messages");
+  res.locals.error_messages = req.flash("error_messages");
+  next();
+});
 
 app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
