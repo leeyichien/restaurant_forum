a9d1dc12f5a7dba187299e89cf618eae6c517225
leeyichien <tdme.tdem@me.com>
2019-08-26 06:04:18 +0800

    建立使用者登入機制

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const db = require("./models");
 const bodyParser = require("body-parser"); // add this
 const flash = require("connect-flash");
 const session = require("express-session");
+const passport = require("./config/passport");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 const port = 3000;
 
 app.use(session({ secret: "secret", resave: false, saveUninitialized: false }));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash("success_messages");
@@ -20,9 +24,10 @@ app.use((req, res, next) => {
 app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+//listen to port 3000
 app.listen(port, () => {
   db.sequelize.sync();
   console.log(`Example app listening on port ${port}!`);
 });
 
-require("./routes")(app);
+require("./routes")(app, passport);
