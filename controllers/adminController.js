f075a40b14c9ef58bef2ea14a704bef1f78dfe1b
leeyichien <tdme.tdem@me.com>
2019-09-06 15:33:11 +0800

    refactor adminController.getRestaurants

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -6,11 +6,12 @@ const fs = require("fs");
 const imgur = require("imgur-node-api");
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID;
 
+const adminService = require("../services/adminService.js");
+
 const adminController = {
   getRestaurants: (req, res) => {
-    return Restaurant.findAll({ include: [Category] }).then(restaurants => {
-      // console.log(restaurants)
-      return res.render("admin/restaurants", { restaurants: restaurants });
+    adminService.getRestaurants(req, res, data => {
+      return res.render("admin/restaurants", data);
     });
   },
   //使用者權限管理
