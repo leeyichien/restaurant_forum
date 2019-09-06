aaf5c4cd1a08a031c40ebe76b4a3ee4a0233402b
leeyichien <tdme.tdem@me.com>
2019-09-06 21:19:20 +0800

    refactor adminController.deleteRestaurant

--- controllers/api/adminController.js
+++ controllers/api/adminController.js
@@ -13,6 +13,13 @@ const adminController = {
     adminService.getRestaurant(req, res, data => {
       return res.json(data);
     });
+  },
+  deleteRestaurant: (req, res) => {
+    adminService.deleteRestaurant(req, res, data => {
+      if (data["status"] === "success") {
+        return res.redirect("/admin/restaurants");
+      }
+    });
   }
 };
 module.exports = adminController;
